19597c7356ccb48dd8e831c9f323c14b97558efe
Jennifer Miller <jmiller@archersystems.us>
2019-12-13 01:01:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,5 +15,43 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.new_by_name(string)
+    song = self.new 
+    song.name = "#{string}"
+    song
+   end
+
+   def self.create_by_name(string)
+     song = self.new
+     song.name = "#{string}"
+     @@all << song
+     song
+   end
+
+   def self.find_by_name(string)
+     @@all.find{|song| song.name == string}
+   end
+
+   #def self.find_by_name(string)
+     #@@all.find{|person| person.name == string}
+   #end
+
+   def self.find_or_create_by_name(string)
+     if self.find_by_name(string) == nil
+       self.create_by_name(string)
+       else 
+         self.find_by_name(string)
+     end
+   end
+
+   def self.alphabetical
+     @@all.sort_by{|song| song.name}
+   end
+ end
+
+
+
+  
 
 end
